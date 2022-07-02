79f7923cc93fb7f77cf71a111861af31677acf22
Lokesh <lokesh.jetty@gmail.com>
2022-07-02 19:04:42 +0530

    initial commit

---
+++ src/arithmaticoperators.java
@@ -0,0 +1,18 @@
+
+public class arithmaticoperators
+{
+public static void main(String args[])
+{
+	int i=4,j=6;
+	int r1=i+j;
+	int r2=i-j;
+	int r3=i*j;
+	double r4=(double)i/j;
+	int r5=i%j;
+	System.out.println(r1);
+	System.out.println(r2);
+	System.out.println(r3);
+	System.out.println(r4);
+	System.out.println(r5);
+}
+}
