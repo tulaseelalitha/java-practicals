3c83978f523818a810e0e3c179a51c5094d5c5a9
Lokesh <lokesh.jetty@gmail.com>
2022-07-06 12:35:11 +0530

    adding all the existing files

---
+++ src/loopoperators.java
@@ -0,0 +1,16 @@
+
+public class loopoperators 
+{
+ public static void main(String args[])
+ {
+	 int i=10,k=20;
+ if(i>k)
+ {
+ System.out.println("i is smallest number");
+ }
+ else
+ { System.out.println("k is largest number");
+ }
+ 
+ } 
+}
---
+++ src/loopoperators2.java
@@ -0,0 +1,29 @@
+
+public class loopoperators2 {
+
+	public static void main(String[] args) 
+	{
+//		int i=1;
+//		while(i<=5)
+//		{
+//			System.out.println("Helloworld");
+//			i++;
+
+//	{
+//		int i=1;
+//		do
+//		{
+//			System.out.println("Helloworld");
+//			i++;
+//		}
+//			while(i<=5);
+//		
+//	    }
+	
+		for(int i=1;i<=5;i++)
+		{
+			System.out.println("Helloworld");
+		}
+	}
+
+}
