855f97f193295848137fdec2480243efecde327b
Lokesh <lokesh.jetty@gmail.com>
2022-07-22 20:47:22 +0530

    addedawtexampleasapartofuserstory

--- src/awtexample.java
+++ src/awtexample.java
@@ -1,6 +1,12 @@
 import java.awt.*;
+import java.awt.frame;
 
-public class awtexample  extends Frame
+public class awtexample  extends Frame{
+public static void main (string[]args)
 {
+ String s1="HELLO";
+ String s2=s1.ToLowercase();
+ System.out.pritln(s2);
  
+}
 }
